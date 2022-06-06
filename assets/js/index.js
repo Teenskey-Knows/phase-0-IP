abaddb678b1b1d5059de48fb90b7eb305f161478
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-06 08:51:16 +0300

    Finishing up

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,13 @@ function validate() {
   const number = document.getElementById("number");
   const message = document.getElementById("message");
 
-  if (name1.value == "" || number.value == "");
+  if (name1.value.trim() == "" || number.value.trim() == "" || message.value.trim() == "")
   {
     alert("Kindly fill out all the required inputs.");
+    return false;
+  }else{
+    alert("Your inquiry has been successfully submitted.We will reply as soon as we are able. Thank you.")
+    true;
+    
   }
 }
